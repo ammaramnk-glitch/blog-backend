6c6fc468381113886a8557bd05a54eab995731dd
Ammara <amarsal2743@gmail.com>
2025-11-15 22:58:37 -0500

    update

--- config/admin.ts
+++ config/admin.ts
@@ -18,3 +18,18 @@ export default ({ env }) => ({
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
 });
+
+
+module.exports = ({ env }) => ({
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+});
